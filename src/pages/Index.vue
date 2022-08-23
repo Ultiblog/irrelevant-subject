a2a94653b57439145d2a2ff894ceccf2d25bfa9f
gsak3l <gsakel5@gmail.com>
2022-08-23 20:45:54 +0300

    Added some more header images. I like randomness in this page

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -101,11 +101,9 @@ export default {
   },
   data() {
     return {
-      images: [
-        'bananaa.jpg',
-        'benjamin-voros-phIFdC6lA4E-unsplash.jpg',
-        'jay-mantri-TFyi0QOx08c-unsplash.jpg',
-        'phoenix-han-Nqdh0G8rdCc-unsplash.jpg'
+      images:['1.jpg', '2.jpg', '3.jpg', '4.jpg', '5.jpg', '6.jpg', '7.jpg',
+        '8.jpg', '9.jpg', '10.jpg', '11.jpg', '12.jpg', '13.jpg', '14.jpg',
+        '15.jpg', '16.jpg'
       ],
     };
   },
